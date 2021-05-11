9d3c581da4d4eb1e9386ee6e449027638b60df8c
Kleverson-c <kleverson.costa@live.com>
2021-05-11 19:49:52 -0300

    Add files via upload

--- src/com/kleverson/json/json.java
+++ src/com/kleverson/json/json.java
@@ -21,27 +21,7 @@ public class json {
     static String json;
 
     public static void main(String[] args) {
-        MongoDB.DBconnect();
-        Document Doc = getAgenda();
-        InsertCheck("agendas", Doc);
-        MongoDB.DocumentCount("agendas");
 
-        new Column()
-                .addLine("id", "nome", "esfera")
-                .addLine(Doc.getInteger("_id").toString(), Doc.getString("nome"), Doc.getString("esfera"))
-                .print()
-        ;
-
-    }
-
-    private static void InsertCheck(String CollectionName, Document Doc){
-        if (MongoDB.findID(CollectionName, Doc.getInteger("_id")) != null) {
-            MongoDB.DeleteOne(CollectionName, eq("_id", Doc.getInteger("_id")));
-
-        }
-        MongoDB.insertOne("agendas",Doc);
-    }
-    private static Document getAgenda(){
         BufferedReader reader;
         String line;
         StringBuffer responseContent = new StringBuffer();
@@ -55,6 +35,7 @@ public class json {
             connection = (HttpURLConnection) url.openConnection();
 
             int status = connection.getResponseCode();
+            String statusMessage = connection.getResponseMessage();
 
             if (status > 299) {
                 reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
@@ -62,40 +43,61 @@ public class json {
                     responseContent.append(line);
                 }
                 reader.close();
+                System.out.println("Status: "+status+" "+statusMessage);
+                System.out.println(responseContent);
             } else {
                 reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 while ((line = reader.readLine()) != null) {
                     responseContent.append(line);
                 }
                 reader.close();
+
+                //CONVERTING FROM STRING TO JSON OBJECT
+                json = responseContent.toString();
+                JSONObject obj = new JSONObject(json);
+
+                //READING JSON ARRAY
+                JSONArray arr = obj.getJSONArray("agenda");
+                //for (int i = 0; i < arr.length(); i++) {
+                Number id = arr.getJSONObject(0).getNumber("@id");
+                String nome = arr.getJSONObject(0).getString("nome");
+                String esfera = arr.getJSONObject(0).getString("esfera");
+
+                agenda = new Document("_id", id)
+                        .append("nome", nome)
+                        .append("esfera", esfera);
+                // }
+
+                Document Doc = agenda;
+
+                MongoDB.DBconnect();
+
+                InsertCheck("agendas", Doc);
+                MongoDB.DocumentCount("agendas");
+
+                new Column()
+                        .addLine("id", "nome", "esfera")
+                        .addLine(Doc.getInteger("_id").toString(), Doc.getString("nome"), Doc.getString("esfera"))
+                        .print();
+
             }
-
-            //CONVERTING FROM STRING TO JSON OBJECT
-            json = responseContent.toString();
-            JSONObject obj = new JSONObject(json);
-
-            //READING JSON ARRAY
-            JSONArray arr = obj.getJSONArray("agenda");
-            //for (int i = 0; i < arr.length(); i++) {
-            Number id = arr.getJSONObject(0).getNumber("@id");
-            String nome = arr.getJSONObject(0).getString("nome");
-            String esfera = arr.getJSONObject(0).getString("esfera");
-
-            agenda = new Document("_id", id)
-                    .append("nome", nome)
-                    .append("esfera", esfera);
-            // }
-
-
-
 
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
+            System.out.println("MalformedURLException");
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("IOException");
         }
-        return agenda;
+
 
     }
-}
+
+    private static void InsertCheck(String CollectionName, Document Doc) {
+        if (MongoDB.findID(CollectionName, Doc.getInteger("_id")) == Doc) {
+            MongoDB.DeleteOne(CollectionName, eq("_id", Doc.getInteger("_id")));
+
+        }
+        MongoDB.insertOne("agendas", Doc);
+    }}
